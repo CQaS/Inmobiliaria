60439d457dd09f95994637ce609ef13c4f561fec
avila Esteban <40841825+CQaS@users.noreply.github.com>
2018-11-28 21:02:26 -0300

    Add files via upload
    columnas Compuestas en Alquiler

---
+++ mi_Inmobiliaria(arreglo).sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-11-2018 a las 00:58:49
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `myinmobiliaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alquileres`
+--
+
+CREATE TABLE `alquileres` (
+  `costo` double NOT NULL,
+  `inicio` datetime NOT NULL,
+  `fin` datetime NOT NULL,
+  `id_inmuebles` int(11) NOT NULL,
+  `id_inquilinos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(20) NOT NULL,
+  `id_duenio` int(11) NOT NULL,
+  `dni` varchar(11) COLLATE utf8_spanish2_ci NOT NULL,
+  `celu` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `Nombres` varchar(35) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inmuebles`
+--
+
+CREATE TABLE `inmuebles` (
+  `id_inmuebles` int(11) NOT NULL,
+  `domicilio` varchar(80) COLLATE utf8_spanish2_ci NOT NULL,
+  `Ambientes` int(11) NOT NULL,
+  `costo` double NOT NULL,
+  `Disponible` tinyint(1) NOT NULL,
+  `id_duenio` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alquileres`
+--
+ALTER TABLE `alquileres`
+  ADD PRIMARY KEY (`id_inmuebles`,`id_inquilinos`),
+  ADD KEY `id_inquilinos` (`id_inquilinos`),
+  ADD KEY `id_inmuebles` (`id_inmuebles`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `id_duenio` (`id_duenio`);
+
+--
+-- Indices de la tabla `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  ADD PRIMARY KEY (`id_inmuebles`),
+  ADD KEY `id_duenio` (`id_duenio`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  MODIFY `id_inmuebles` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alquileres`
+--
+ALTER TABLE `alquileres`
+  ADD CONSTRAINT `alquileres_ibfk_1` FOREIGN KEY (`id_inquilinos`) REFERENCES `clientes` (`id_cliente`),
+  ADD CONSTRAINT `alquileres_ibfk_2` FOREIGN KEY (`id_inmuebles`) REFERENCES `inmuebles` (`id_inmuebles`);
+
+--
+-- Filtros para la tabla `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  ADD CONSTRAINT `inmuebles_ibfk_1` FOREIGN KEY (`id_duenio`) REFERENCES `clientes` (`id_duenio`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
